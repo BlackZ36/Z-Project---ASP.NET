f7eeb47853c7d24c79355aa0e2ae45d8cb7edcab
BlackZ <duchinh0306@gmail.com>
2024-05-15 22:55:24 +0700

    Initial commit

---
+++ ASP.NET MVC Web App - VIEW/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ASP.NET_Core___Reference;
+using ASP.NET_MVC_Web_App___VIEW.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace ASP.NET_MVC_Web_App___VIEW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        async public Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetAsync("https://localhost:7188/WeatherForecast");
+
+            var res = await data.Content.ReadAsStringAsync();
+            var dataJson = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(res);
+
+            return View(dataJson);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET MVC Web App - VIEW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_MVC_Web_App___VIEW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
